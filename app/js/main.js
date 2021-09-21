1eb6d50520c2b55af5be6863199174c79d1c43e3
dima <dima.xipkov@gmail.com>
2021-09-21 21:26:42 +0300

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,28 @@
+$(function name(params) {
+
+
+
+    /*	$(".menu a").on("click", function (event) {
+    	    event.preventDefault();
+    	    var id = $(this).attr('href'),
+    	        top = $(id).offset().top;
+    	    $('body,html').animate({
+    	        scrollTop: top
+    	    }, 1500);
+    	});
+
+        /*для закрыть меню*/ /*
+        $('.menu__btn').on('click', function () {
+            $('.menu__btn').toggleClass('active');
+        });
+        
+         $('.home__slider').slick({
+             dots: true,
+             arrows: false,
+         });
+        
+        
+        */
+
+
+});
